5a3415ffaff261c9f65e6c218e0f309accd9d4b2
KerritoIslam <mi.amour@esi-sba.dz>
2024-12-20 16:50:25 +0100

    lieutouristoique

--- lib/features/Servises/pages/home.dart
+++ lib/features/Servises/pages/home.dart
@@ -1,6 +1,6 @@
-
 import 'package:bsc_app/features/Servises/pages/homesubpages/all.dart';
 import 'package:bsc_app/features/Servises/pages/homesubpages/hebergement.dart';
+import 'package:bsc_app/features/Servises/pages/homesubpages/lieu_touristique.dart';
 import 'package:bsc_app/features/Servises/pages/homesubpages/services.dart';
 import 'package:bsc_app/features/Servises/pages/homesubpages/transport.dart';
 
@@ -19,7 +19,6 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
     super.initState();
 
     tabController = TabController(length: 5, vsync: this);
-
   }
 
   @override
@@ -30,7 +29,6 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-
     return Column(
       children: [
         Padding(
@@ -52,43 +50,48 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
           controller: tabController,
           isScrollable: true,
           labelStyle: TextStyle(
-    fontSize: 16, // Bigger font size for selected tab
-    fontWeight: FontWeight.bold, 
-    color: Colors.black// Bold style for selected tab
-  ),
-  unselectedLabelStyle: TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeight.normal
-  ),
+              fontSize: 16, // Bigger font size for selected tab
+              fontWeight: FontWeight.bold,
+              color: Colors.black // Bold style for selected tab
+              ),
+          unselectedLabelStyle:
+              TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
           labelPadding:
               EdgeInsets.symmetric(horizontal: 8.0), // Reduced spacing
           tabs: [
             Tab(
-                child: Text('All',
-                    )),
+                child: Text(
+              'All',
+            )),
             Tab(
-                child: Text('Service',
-                    )),
+                child: Text(
+              'Service',
+            )),
             Tab(
-                child: Text('hebergement',
-                    )),
+                child: Text(
+              'hebergement',
+            )),
             Tab(
-                child: Text('Transport',
-                    )),
+                child: Text(
+              'Transport',
+            )),
             Tab(
-                child: Text('Lieu Touristique',
-                    )),
+                child: Text(
+              'Lieu Touristique',
+            )),
           ],
         ),
         Expanded(
           child: TabBarView(
             controller: tabController,
             children: [
-              All_page(tabController: tabController,),
+              All_page(
+                tabController: tabController,
+              ),
               Services(),
               Hebergement(),
               Transport(),
-              All_page(tabController: tabController,),
+              LieuxTouristique(),
             ],
           ),
         ),
--- lib/features/Servises/pages/homesubpages/lieu_touristique.dart
+++ lib/features/Servises/pages/homesubpages/lieu_touristique.dart
@@ -7,9 +7,54 @@ class LieuxTouristique extends StatefulWidget {
   State<LieuxTouristique> createState() => _LieuxTouristiqueState();
 }
 
+List<Map<String, dynamic>> _LieuxTouristique = [
+  {
+    'name': 'deles',
+    'map_position': 'link',
+    'position': 'boumerdes',
+  },
+  {
+    'name': 'corso',
+    'map_position': 'link',
+    'position': 'boumerdes',
+  },
+  {
+    'name': 'darchabab',
+    'map_position': 'link',
+    'position': 'boumerdes',
+  }
+];
+
 class _LieuxTouristiqueState extends State<LieuxTouristique> {
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      itemCount: _LieuxTouristique.length,
+      itemBuilder: (context, index) {
+        return Container(
+          height: 123,
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          decoration: BoxDecoration(
+            color: Color(0xFFD9D9D9),
+            borderRadius: BorderRadius.circular(28),
+          ),
+          child: ListTile(
+            subtitle: Padding(
+              padding: const EdgeInsets.only(left: 74),
+              child: Text(
+                  'type: ' +
+                      _LieuxTouristique[index]['position'] +
+                      '\nposition: ' +
+                      _LieuxTouristique[index]['map_position'],
+                  style: TextStyle(fontSize: 13, color: Colors.grey[800])),
+            ),
+            title: Text(_LieuxTouristique[index]['name'],
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24, color: Colors.grey[800])),
+          ),
+        );
+      },
+    );
   }
 }
