924c95f40c7324176f57f4ddfcb7f5c2e567d8dd
KerritoIslam <mi.amour@esi-sba.dz>
2024-12-20 15:52:54 +0100

    map

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-
-
+import 'package:bsc_app/features/Map/pages/map.dart';
 import 'package:bsc_app/features/Servises/pages/navigation.dart';
-
 
 import 'package:flutter/material.dart';
 
@@ -13,10 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       debugShowCheckedModeBanner: false,
       theme: ThemeData(splashColor: Colors.transparent),
-
       home: HomePage(),
     );
   }
